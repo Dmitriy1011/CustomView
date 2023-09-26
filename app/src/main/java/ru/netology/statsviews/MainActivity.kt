444e66385c6e915c8ad58032bb1dd9d0520c76de
Dmitriy Prishchepa <prishchepadmitriy@gmail.com>
2023-09-26 14:53:38 +0300

    l_tr_2 changes

--- app/src/main/java/ru/netology/statsviews/MainActivity.kt
+++ app/src/main/java/ru/netology/statsviews/MainActivity.kt
@@ -1,11 +1,13 @@
 package ru.netology.statsviews
 
+import android.animation.LayoutTransition
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.ChangeBounds
 import android.transition.Scene
 import android.transition.TransitionManager
 import android.view.ViewGroup
+import android.view.animation.BounceInterpolator
 import android.widget.Button
 import ru.netology.statsview.ui.StatsView
 
@@ -15,12 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val root = findViewById<ViewGroup>(R.id.root)
-        val scene = Scene.getSceneForLayout(root, R.layout.end_scene, this)
+        root.layoutTransition = LayoutTransition().apply {
+            setDuration(2000)
+            setInterpolator(LayoutTransition.CHANGE_APPEARING, BounceInterpolator())
+        }
 
-        findViewById<Button>(R.id.goButton).setOnClickListener {
-            TransitionManager.go(scene, ChangeBounds().apply {
-                duration = 1000
-            })
+        val btnGo = findViewById<Button>(R.id.goButton).setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.stats_view, root, false)
+            root.addView(view, 0)
         }
     }
 }
