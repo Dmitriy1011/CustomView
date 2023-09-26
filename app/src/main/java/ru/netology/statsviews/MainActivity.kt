f5d4879c441cf69ea9082a76f46256192443425a
Dmitriy Prishchepa <prishchepadmitriy@gmail.com>
2023-09-26 11:50:01 +0300

    layout_transition

--- app/src/main/java/ru/netology/statsviews/MainActivity.kt
+++ app/src/main/java/ru/netology/statsviews/MainActivity.kt
@@ -2,14 +2,25 @@ package ru.netology.statsviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.ChangeBounds
+import android.transition.Scene
+import android.transition.TransitionManager
+import android.view.ViewGroup
+import android.widget.Button
 import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data = listOf(
-            0.25F, 0.25F, 0.25F
-        )
+
+        val root = findViewById<ViewGroup>(R.id.root)
+        val scene = Scene.getSceneForLayout(root, R.layout.end_scene, this)
+
+        findViewById<Button>(R.id.goButton).setOnClickListener {
+            TransitionManager.go(scene, ChangeBounds().apply {
+                duration = 1000
+            })
+        }
     }
 }
